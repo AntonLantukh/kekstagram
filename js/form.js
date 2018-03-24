5d91175d1bdde9b780e4a949c6179a16656c24bd
Anton Lantukh <lantukh@bk.ru>
2018-03-24 19:58:15 +0400

    Подшотовил загрузку своих фото

--- js/form.js
+++ js/form.js
@@ -4,8 +4,10 @@
 
   // Работа с формой
   var ESC_KEYCODE = 27;
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var downloadFile = document.querySelector('#upload-file');
+  var preview = document.querySelector('.effect-image-preview');
   var closeForm = document.querySelector('.upload-form-cancel');
   var formWindow = document.querySelector('.upload-overlay');
   var form = document.querySelector('.upload-form');
@@ -18,6 +20,16 @@
     formWindow.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPressForm);
     scaleFull.setAttribute('style', 'display: none');
+
+    var file = downloadFile.files[0];
+    var fileName = file.name.toLowerCase();
+    var mathes = FILE_TYPES.some(function (item) {
+      return fileName.endsWith(item);
+    });
+
+    if (mathes) {
+      insertPicture(file);
+    }
   });
 
   // Закрытие формы при клике на крестик
@@ -33,10 +45,27 @@
     }
   }
 
+  // Сабмит формы
   form.addEventListener('submit', function (evt) {
     evt.preventDefault();
     window.backend.save(new FormData(form), function () {
       form.reset();
     }, window.backend.onErrorCallback);
   });
+
+  // Функция вставки фото пр загрузке
+  function insertPicture(file) {
+    var filterPreview = document.querySelectorAll('.upload-effect-preview');
+    var reader = new FileReader();
+    reader.addEventListener('load', function () {
+      preview.setAttribute('src', reader.result);
+      preview.setAttribute('width', 586);
+      preview.setAttribute('height', 587);
+
+      filterPreview.forEach(function (item, i, arr) {
+        item.setAttribute('style', 'background-image: url("' + reader.result + '");');
+      });
+    });
+    reader.readAsDataURL(file);
+  }
 })();
